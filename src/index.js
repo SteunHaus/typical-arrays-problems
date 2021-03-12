2c2d817f63d64c430f7b9a92ea531a051a248406
SteunHaus <pavel.evdokimenko@gmail.com>
2021-03-12 22:56:00 +0300

    fix: Update index.js with valid solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let minimal = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[minimal]) minimal = i;
+  }
+  return array[minimal];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let maximal = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[maximal]) maximal = i;
+  }
+  return array[maximal];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let average = 0;
+  for (let i = 0; i < array.length; i++) {
+    average = average + array[i];
+  }
+  return average/array.length;
 }
